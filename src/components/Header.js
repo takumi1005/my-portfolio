5f7bc68d61e145935f0444fd3ad59c2fb9f14b6d
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-07-12 16:35:10 +0900

    内容編集

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,7 @@ const Header = props => (
       <div className="inner">
         <h1>Dimension</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          紹介文
         </p>
       </div>
     </div>
